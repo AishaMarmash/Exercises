02a2b12f8e48265719b718526fc7bfdda956e5ca
Aisha Marmash <aishamarmash@gmail.com>
2022-03-01 13:01:11 +0200

    add Exercise 3 solution

---
+++ Exercise3/Exercise3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Exercise3Application
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var query = numbers.Select(x => x).Transform(x => x + 1);
+            foreach (var number in query)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+
+    public delegate int MyFun(int num);
+
+    public static class MyClass
+    {
+        public static IEnumerable<int> Transform(this IEnumerable<int> data, MyFun func)
+        {
+            foreach (var i in data)
+            {
+                yield return func(i);
+            }
+        }
+    }
+}
+
